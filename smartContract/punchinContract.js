da105b01d06ad6d1b258012f0c459969d12a8081
Ruter <ruter.lv@gmail.com>
2018-05-12 16:05:10 +0800

    init contract

---
+++ smartContract/punchinContract.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var PunchInContract = function () {
+    LocalContractStorage.defineProperties(this, {
+
+    });
+    LocalContractStorage.defineMapProperty(this, '');
+};
+
+PunchInContract.prototype = {
+
+    init: function () {
+
+    }
+};
+
+module.exports = PunchInContract;
