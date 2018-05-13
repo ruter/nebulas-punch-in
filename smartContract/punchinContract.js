3472548c4841f178b755cbf7a625026bc5c3a07f
Ruter <ruter.lv@gmail.com>
2018-05-13 12:25:59 +0800

    update contract

--- smartContract/punchinContract.js
+++ smartContract/punchinContract.js
@@ -4,11 +4,13 @@ var PunchInContract = function () {
     LocalContractStorage.defineProperties(this, {
         size: null,
         superuserAddress: null,
-        transferLimit: null,
+        transferLimit: null
     });
     LocalContractStorage.defineMapProperty(this, 'tasks');
+    LocalContractStorage.defineMapProperty(this, 'validTasks');
     LocalContractStorage.defineMapProperty(this, 'deposit');
     LocalContractStorage.defineMapProperty(this, 'operation');
+    LocalContractStorage.defineMapProperty(this, 'taskByOwner');
 };
 
 PunchInContract.prototype = {
@@ -31,12 +33,86 @@ PunchInContract.prototype = {
             info.type = 1;
         }
         opArray.push(info);
-        this.operation.put(hash, opArray)
+        this.operation.put(hash, opArray);
     },
 
-    // 创建打卡
+    _setTaskByOwner: function (hash) {
+        var from = Blockchain.transaction.from,
+            taskByOwner = this.taskByOwner.get(from) || [];
+        taskByOwner.push(hash);
+        this.taskByOwner.put(from, taskByOwner);
+    },
+
+    _getPunchState: function (hash, createTime) {
+        /**
+         * @param hash {string} task's hash
+         * @param createTime {Date} task's create date
+         * return state {int} 1: punched, 0: should punch, -1: failed
+         */
+        var opArr = this.operation.get(hash),
+            punchDays = opArr.length - 1,
+            now = new Date(),
+            startDate = new Date(createTime),
+            DAYMS = 86400000;
+        var start = [startDate.getFullYear(), startDate.getMonth() + 1, startDate.getDate()].join('-'),
+            end = [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('-');
+        var delta = (new Date(end) - new Date(start)) / DAYMS;
+        if (delta < punchDays) {
+            return 1;
+        } else if (delta === punchDays) {
+            return 0;
+        } else {
+            return -1;
+        }
+    },
+
+    _setValidTasks: function (hash) {
+        var from = Blockchain.transaction.from,
+            validTask = this.validTasks.get(from) || [],
+            newValidTask = [hash],
+            amount = new BigNumber(0);
+        for (var i = 0; i < validTask.length; i++) {
+            var key = validTask[i],
+                task = this.tasks.get(key),
+                state = this._getPunchState(key, task.datetime);
+            if (state !== -1) {
+                newValidTask.push(key);
+            } else {
+                amount = amount.plus(task.deposit);
+            }
+        }
+        var deposit = this.deposit.get(from);
+        if (!deposit) {
+            deposit = { balance: new BigNumber(0)};
+        }
+        if (amount.gt(0) && amount.lte(deposit.balance)) {
+            deposit.balance = deposit.balance.minus(amount);
+            this.deposit.put(from, deposit);
+        }
+        this.validTasks.put(from, newValidTask);
+    },
+
+    _setDepositBalance: function (value) {
+        var from = Blockchain.transaction.from,
+            deposit = this.deposit.get(from);
+        if (!deposit) {
+            deposit = { balance: new BigNumber(0)};
+        }
+        var balance = new BigNumber(deposit.balance);
+        deposit.balance = balance.plus(value);
+        this.deposit.put(from, deposit);
+    },
+
+    getUserAddress: function () {
+        return Blockchain.transaction.from;
+    },
+
+    // create task
     create: function (info) {
         this.size++;
+        if (info.cycle < 3) {
+            throw new Error("Invalid Cycle");
+        }
         var from = Blockchain.transaction.from,
             hash = Blockchain.transaction.hash,
             amount = Blockchain.transaction.value,
@@ -46,16 +122,94 @@ PunchInContract.prototype = {
                 cycle: info.cycle,
                 deposit: amount,
                 hash: hash,
-                datetime: Date.now()
-            },
-            tasks = this.tasks.get(from) || [];
-        tasks.push(task);
-        this.tasks.put(from, tasks);
+                datetime: Date.now(),
+                from: from
+            };
+        this.tasks.put(hash, task);
+        this._setValidTasks(hash);
+        this._setTaskByOwner(hash);
+        this._setOperation(hash, true);
+        if (amount.gt(0)) {
+            this._setDepositBalance(amount);
+        }
+        Event.Trigger('createTask', {
+            hash: hash,
+            amount: amount
+        });
     },
 
-    // 打卡
+    // punch in
     punch: function (hash) {
+        var from = Blockchain.transaction.from,
+            task = this.tasks.get(hash);
+        if (!task) {
+            throw new Error("Invalid Hash");
+        }
+        if (from !== task.from) {
+            throw new Error("Permission Denied");
+        }
+        var state = this._getPunchState(hash, task.datetime);
+        if (state === -1) {
+            throw new Error("Task is Failed");
+        } else if (state === 0) {
+            this._setOperation(hash, false);
+        } else {
+            throw new Error("Already Punched In");
+        }
+        Event.Trigger('punchIn', {
+            hash: hash,
+            datetime: Date.now()
+        });
+    },
 
+    // get owner's tasks
+    getTasksByOwner: function (page, limit) {
+        if (!page) {
+            page = 1;
+        }
+        if (!limit) {
+            limit = 10;
+        }
+        var from = Blockchain.transaction.from,
+            tasks = this.taskByOwner.get(from) || [],
+            allTasks = [],
+            len = tasks.length,
+            maxPage = Math.ceil(len / limit),
+            start = (page - 1) * limit,
+            end = page * limit;
+        if (maxPage < page) {
+            throw new Error("No More Data");
+        }
+        if (end > len) {
+            end = len;
+        }
+        for (var i = start; i < end; i++) {
+            var tmpTask = this.tasks.get(tasks[i]);
+            allTasks.push(tmpTask);
+        }
+        return allTasks;
+    },
+
+    // get valid tasks
+    getValidTasks: function () {
+        var from = Blockchain.transaction.from,
+            validTask = this.validTasks.get(from) || [],
+            tasks = [];
+        for (var i = 0; i < validTask.length; i++) {
+            var task = this.tasks.get(validTask[i]);
+            tasks.push(task);
+        }
+        return tasks;
+    },
+
+    // get task by hash
+    getTaskByHash: function (hash) {
+        return this.tasks.get(hash);
+    },
+
+    // get punch in detail by hash
+    getPunchDetail: function (hash) {
+        return this.operation.get(hash);
     },
 
     transfer: function (value) {
